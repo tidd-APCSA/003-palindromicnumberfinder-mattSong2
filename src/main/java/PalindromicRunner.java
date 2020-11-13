1205471fefe4c70d864c7120121d47e2e82d2685
mattSong2 <msong1012@gmail.com>
2020-11-13 20:30:03 +0000

    first attempt completion

--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,6 +1,7 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
         // be sure you're testing each method as you work then produce your final output using the toString method
-
+        PalindromicNumberFinder test = new PalindromicNumberFinder(121);
+        System.out.println(test);
     }
 }
